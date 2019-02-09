af529399f9570924736c7a37f6ed9bd42d04f79b
Ryan Kim <skim19@stuy.edu>
2019-02-09 17:12:55 -0500

    updated code

--- base_python/draw.py
+++ base_python/draw.py
@@ -3,41 +3,89 @@ from display import *
 def draw_line( x0, y0, x1, y1, screen, color ):
 
     A = y1 - y0
-    B = x1 - x0
-    X = x0, Y = y0, d = 2A + B
-    
+    B = x0 - x1
+    m = -A/B
+    X = x0
+    Y = y0
+    d = 2*A + B
+
+    if m > 0:
+        if m > 1:
+            #octant 2           
+            d = A + 2*B
+            while Y <= y1:
+                plot(screen, color, X, Y)
+                if d < 0:
+                    X+=1
+                    d += 2*A
+                Y+=1
+                d += 2*B
+        else:
+            #octant 1
+            d = 2*A + B
+            while X <= x1:
+                plot(screen, color, X, Y)
+                if d > 0:
+                    Y+=1
+                    d += 2*B
+                X+=1
+                d += 2*A
+    else:
+        if m < -1:
+            #octant 7
+            d = -1*A - 2*B
+            while Y >= y1:
+                plot(screen, color, X, Y)
+                if d > 0:
+                    X-=1
+                    d += 2*B
+                Y-=1
+                d += 2*A
+        else:
+            #octant 8
+            d = -2*A - B
+            while X >= x1:
+                plot(screen, color, X, Y)
+                if d < 0:
+                    Y-=1
+                    d += 2*A
+                X-=1
+                d += 2*B
+
+'''        
     #octant 1
     while X <= x1:
-        plot(x, y)
+        plot(screen, color, X, Y)
         if d > 0:
-            Y++
-            d += 2B
-        X++
-        d += 2A
+            Y+=1
+            d += 2*B
+        X+=1
+        d += 2*A
 
     #octant 2
     while Y <= y1:
-        plot(x, y)
+        plot(screen, color, X, Y)
         if d < 0:
-            X++
-            d += 2A
-        Y++
-        d += 2B
+            X+=1
+            d += 2*A
+        Y+=1
+        d += 2*B
 
     #octant 7
     while Y <= y1:
-        plot(x, y)
+        plot(screen, color, X, Y)
         if d > 0:
-            Y--
-            d -= 2B
-        X--
-        d -= 2A
+            Y-=1
+            d -= 2*B
+        X-=1
+        d -= 2*A
 
     #octant 8
     while Y <= y1:
-        plot(x, y)
+        plot(screen, color, X, Y)
         if d < 0:
-            X--
-            d -= 2A
-        Y--
-        d -= 2B
+            X-=1
+            d -= 2*A
+        Y-=1
+        d -= 2*B
+'''
--- base_python/main.py
+++ base_python/main.py
@@ -2,9 +2,31 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color1 = [ 255, 0, 0 ]
+color2 = [ 0, 255, 0 ]
+color7 = [ 0, 0, 255 ]
+color8 = [ 255, 255, 0 ]
+slope1 = [ 255, 0, 255 ]
+slope0 = [ 0, 255, 255 ]
+slopen1 = [ 255, 255, 255 ]
+slopeun = [ 100, 100, 100 ] 
 
-draw_line(0, 0, 500, 500, screen, color)
+#octant 1 PASS
+#draw_line(0, 0, 500, 250, screen, color1)
+#octant 2 PASS
+#draw_line(0, 0, 250, 500, screen, color2)
+#octant 7
+draw_line(0, 500, 250, 0, screen, color7)
+#octant 8
+draw_line(0, 500, 500, 250, screen, color8)
+#slope 1
+#draw_line(0, 0, 500, 500, screen, slope1)
+#slope 0
+#draw_line(0, 250, 500, 250, screen, slope0)
+#slope -1
+#draw_line(0, 500, 500, 0, screen, slopen1)
+#slope undefined
+#draw_line(0, 0, 0, 500, screen, slopeun)
 
 display(screen)
 save_extension(screen, 'img.png')
